6a9f78c629ea7e5d41f7efad32064ed5ad95e781
jimmyhhuang <jimmyhhuang@tencent.com>
2025-11-13 11:46:46 +0800

    feat[demo] flink 1.13.6 wordcount 例子

---
+++ doc/socket-demo.md
@@ -0,0 +1,6 @@
+## mac启动netcat
+### 1.下载
+brew install netcat
+
+### 2.mac启动
+nc -l -p 9000
---
+++ flink-1.13/src/main/java/flink/demo/sockect/SocketTextStreamWordCount.java
@@ -0,0 +1,43 @@
+package flink.demo.sockect;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+
+public class SocketTextStreamWordCount {
+    public static void main(String[] args) throws Exception {
+
+        // 创建 streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 获取数据
+        DataStreamSource<String> stream = env.socketTextStream("10.91.77.118", 9000);
+
+        // 计数
+        SingleOutputStreamOperator<Tuple2<String, Integer>> sum = stream.flatMap(new LineSplitter())
+                .keyBy(0)
+                .sum(1);
+
+        sum.print();
+
+        env.execute("Java WordCount from SocketTextStream Example");
+    }
+
+    public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) {
+            String[] tokens = s.toLowerCase().split("\\W+");
+
+            for (String token: tokens) {
+                if (token.length() > 0) {
+                    collector.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
+}
+
